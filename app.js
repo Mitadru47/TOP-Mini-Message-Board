9b8e75181cee5dd6a4f4bc681fb0a70f3cd51089
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-01-21 14:18:00 +0530

    Minor Tweaks

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
 const http = require('http');
 const server = http.createServer(app);
 
 const port = 3000;
 server.listen(port, () => console.log('Listening on ' + port));
-
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
