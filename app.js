50a11139b4bc218149dfbba5e92a1e38ac0a9f66
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-01-23 16:34:10 +0530

    Dummy Data & View Engine Setup

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// view engine setup
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,28 @@
-const express = require("express");
+const messages = [
+    
+    {
+      text: "Hi there!",
+      user: "Amando",
 
+      added: new Date()
+    },
+
+    {
+      text: "Hello World!",
+      user: "Charles",
+
+      added: new Date()
+    }
+ ];
+
+const express = require("express");
 const router = express.Router();
 
 router.get("/", function (req, res){
     
-    res.write("This is Express");
+    // res.write("This is Express");
+
+    res.render('index', { title: "Mini Messageboard", messages: messages });
     res.end();
 });
 
