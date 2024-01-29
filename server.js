6b8bf8390a2d4cce459173bc883a59a10e368562
Mitadru <Mitadru.Gangopadhyay@aot.edu.in>
2024-01-29 16:12:33 +0530

    Render Deployment Prep

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require("./app.js");
 
 const server = http.createServer(app);
 
-const hostname = "127.0.0.1";
+const hostname = "0.0.0.0";
 const port = 3000;
 
 server.listen(port, hostname, () => {
